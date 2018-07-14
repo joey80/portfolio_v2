893e0c0e747f7188e72b9910ae692a0dbf44ba89
Joey Leger <legerinvestments@gmail.com>
2018-07-13 20:45:27 -0400

    first build

---
+++ src/scripts/components/app/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <span class="test">hello VUE!</span>
+      <hello-div></hello-div>
+  </div>
+</template>
+
+<script>
+  import { eventBus, state } from '../../main';
+  import Hello from '../hello/Hello.vue';
+
+  export default {
+    components: {
+        helloDiv: Hello,
+    },
+    data: function() {
+      return {
+      }
+    },
+    mounted() {
+    }
+  };
+</script>
+
+<style lang="less">
+    @import '../../global/_variables.less';
+    @import 'styles/app.less';
+</style>
---
+++ src/scripts/components/hello/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <span class="poop">I'm the hello component</span>
+  </div>
+</template>
+
+<script>
+  import { eventBus, state } from '../../main';
+
+  export default {
+    components: {
+    },
+    data: function() {
+      return {
+      }
+    },
+    mounted() {
+    }
+  };
+</script>
+
+<style lang="less">
+    @import 'styles/hello.less';
+</style>
