8b17f367ba6542985291b0b2fe4061495b9189c7
Joey Leger <legerinvestments@gmail.com>
2018-07-19 21:57:16 -0400

    latest build

--- src/scripts/components/home/Home.vue
+++ src/scripts/components/home/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
       <div class="home__message">
           Hello.<br />
-          My name is <span class="home--stronger">Joey Leger</span><br />
-          I'm a full-stack developer.
+          my name is <span class="home--stronger">Joey Leger</span><br />
+          and I'm a full-stack developer.
       </div>
       <div class="home__line"></div>
       <router-link to="/projects"><div class="btn home__btn">Check Out My Work <font-awesome-icon icon="long-arrow-alt-right" class="home__arrow"/></div></router-link>
