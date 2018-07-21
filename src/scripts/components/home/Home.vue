a8bb30f23a27321db369a5894f9516e650a9ed50
Joey Leger <legerinvestments@gmail.com>
2018-07-20 21:53:06 -0400

    added about me section

--- src/scripts/components/home/Home.vue
+++ src/scripts/components/home/Home.vue
@@ -9,11 +9,11 @@
       <div class="home__line"></div>
       <div class="home__skills">
           <p>I am a self-taught developer currently living just outside of <span class="home--stronger">Charlottesville, Virginia.</span> I love
-          front-end development (<span class="home__skills--highlight">HTML</span>, <span class="home__skills--highlight">CSS</span>,
-            <span class="home__skills--highlight">Javascript</span>) because it deals with design, UI/UX and getting to work with JavaScript
+          front-end development - <span class="home__skills--highlight">HTML</span>, <span class="home__skills--highlight">CSS</span>,
+            <span class="home__skills--highlight">Javascript</span> - because it deals with design, UI/UX and getting to work with JavaScript
             libraries like <span class="home__skills--highlight">Vue js</span>. My focus is always on a mobile first experience and making sure
-            that the details scale up nicely. I enjoy back-end development (<span class="home__skills--highlight">PHP</span>,
-            <span class="home__skills--highlight">Python</span>, <span class="home__skills--highlight">Express</span>) because this is
+            that the details scale up nicely. I enjoy back-end development - <span class="home__skills--highlight">PHP</span>,
+            <span class="home__skills--highlight">Python</span>, <span class="home__skills--highlight">mySQL</span>, <span class="home__skills--highlight">Express</span> - because this is
             where all of the functionality is. With front-end work it’s fun to design the look of the car but with back-end I like to get my
             ‘hands greasy’ in the engine creating databases and API’s.</p>
       </div>
