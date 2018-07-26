ff1f2f236c5b1e51b2f60cd10092ee497ca9751c
Joey Leger <legerinvestments@gmail.com>
2018-07-25 22:44:05 -0400

    latest build

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(helmet());
 app.use(serveStatic(__dirname, { index: 'index.html' }));
 //app.use(serveStatic(__dirname + "/dist"));
 
+app.get('/projects', function(req, res) {
+    res.sendFile(path.join(__dirname, '/projects'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  });
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 
