2dd363fc0249adf8791099db06dee214aec780ff
Joey Leger <legerinvestments@gmail.com>
2019-01-15 12:47:53 -0500

    latest build

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express'),
     compression = require('compression'),
     serveStatic = require('serve-static'),
     helmet = require('helmet'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(helmet());
 app.use(serveStatic(__dirname, { index: 'index.html' }));
 
-var port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('server started ' + port);
