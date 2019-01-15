3fdd2ed293f83e4eab40f43cf9d1bbd611b5d6ec
Joey Leger <legerinvestments@gmail.com>
2019-01-15 12:43:13 -0500

    [UPDATE] Added compression to Express server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var helmet = require('helmet');
+const express = require('express'),
+    compression = require('compression'),
+    serveStatic = require('serve-static'),
+    helmet = require('helmet'),
+    app = express();
 
-app = express();
+app.use(compression());
 app.use(helmet());
 app.use(serveStatic(__dirname, { index: 'index.html' }));
-//app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
