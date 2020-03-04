11ec03d9b85819d55b7aed50a081d11e832a7126
Joey Leger <joseph.leger@duke-energy.com>
2020-03-03 19:56:45 -0500

    moved sidebar links into list

--- src/scripts/components/sidebar/Sidebar.vue
+++ src/scripts/components/sidebar/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sidebar">
+  <aside class="sidebar">
     <div class="sidebar__links">
       <ul>
         <li><router-link to="/">Home</router-link></li>
@@ -8,33 +8,45 @@
       </ul>
     </div>
     <div class="sidebar__quote">
-      I'm a creative and passionate full-stack developer that looks forward to taking on new challenges and to
+      I'm a creative and passionate full-stack developer that looks forward to taking on new
+      challenges and to
       <span class="sidebar--stronger">never stop learning new things</span>
     </div>
     <div class="sidebar__social">
-      check out my <span class="sidebar--stronger"><a href="https://github.com/joey80" target="_blank">GitHub</a></span><br />
-      connect with me on <span class="sidebar--stronger"><a href="https://www.linkedin.com/in/joey-leger-363715a5/" target="_blank">LinkedIn</a></span>
+      <ul>
+        <li>
+          check out my
+          <span class="sidebar--stronger"
+            ><a href="https://github.com/joey80" target="_blank">GitHub</a></span
+          >
+        </li>
+        <li>
+          connect with me on
+          <span class="sidebar--stronger"
+            ><a href="https://www.linkedin.com/in/joey-leger-363715a5/" target="_blank"
+              >LinkedIn</a
+            ></span
+          >
+        </li>
+      </ul>
     </div>
     <div class="sidebar__avatar"></div>
-  </div>
+  </aside>
 </template>
 
 <script>
-  import { eventBus, state } from '../../main';
+import { eventBus, state } from '../../main';
 
-  export default {
-    components: {
-    },
-    data: function() {
-      return {
-      }
-    },
-    mounted() {
-    }
-  };
+export default {
+  components: {},
+  data: function() {
+    return {};
+  },
+  mounted() {}
+};
 </script>
 
 <style lang="less">
-  @import '~globalLess';
-  @import 'styles/sidebar.less';
+@import '~globalLess';
+@import 'styles/sidebar.less';
 </style>
